35bdc875cd444c18cf648a9a46b99a91650cb182
aswanthabam <aswanth.abam@gmail.com>
2023-11-06 17:49:21 +0530

    feat : Text Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import IconButton from './components/buttons/IconButton/IconButton'
+import TextButton from './components/buttons/TextButton/TextButton'
 
 function App() {
   return (
     <div className='app dark'>
-      <IconButton icon='bi bi-door-open' text='login' type='secondary'/>
+      <IconButton icon='bi bi-door-open' text='login' type='primary'/>
     </div>
   )
 }
--- src/components/buttons/TextButton/TextButton.tsx
+++ src/components/buttons/TextButton/TextButton.tsx
@@ -1,5 +1,22 @@
 import './TextButton.css';
-
-export default function TextButton() {
-  return <></>
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'boxicons/css/boxicons.css'
+import React from 'react';
+interface TextButtonProps {
+  text: string;
+  icon : string | null;
+  type : string;
 }
+
+const TextButton : React.FC<TextButtonProps> = ({icon,text,type}) => {
+  return <div className={`text-button type-` + type }>
+    <div className='text'>
+      <span>{text}</span>
+    </div>
+    { icon && <div className='icon'>
+      <i className={icon}></i>
+    </div>}
+  </div>;
+}
+
+export default TextButton;
