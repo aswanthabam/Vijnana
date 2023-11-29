25445704c18facee9e73bf2f24cc55d63add6cc4
Aswanth Vc <aswanth.abam@gmail.com>
2023-11-29 09:50:28 +0530

    api optimization

--- src/apis/api.tsx
+++ src/apis/api.tsx
@@ -1,16 +1,29 @@
 import axios from "axios";
 import { _EventInfo } from "../types";
 export const publicRouter = axios.create({ baseURL: "http://localhost:8000" });
+interface EventInfoData {
+  event_id: string;
+  name: string;
+  description: string;
+  link: string;
+  img: string;
+  details: string;
+  documents: string;
+  date: Date;
+  venue: string;
+  poster: string;
+  is_open: boolean;
+}
 export const getEvents = async (eventId: string | null | undefined) => {
   if (eventId) {
     var res = await publicRouter.get("/api/v2/events/?event_id=" + eventId);
   } else {
     var res = await publicRouter.get("/api/v2/events/");
   }
-  var d2: Array<_EventInfo> = [];
+  var d2: _EventInfo[] = [];
   if (res.data["status"]) {
     if (res.data["data"] != undefined && res.data["data"] != null) {
-      var data: [] = res.data["data"];
+      var data: EventInfoData[] = res.data["data"] as EventInfoData[];
       //   console.log(data);
       for (var i = 0; i < data.length; i++) {
         var data2 = data[0];
--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -1,7 +1,5 @@
 import style from "./Events.module.css";
 // import SecondaryButton from '../../components/buttons/secondary_button/SecondaryButton';
-import alien from "../../assets/dehill-spacelove-1-dribble.gif";
-import Counter from "../../components/counter/Counter";
 import EventList from "../../components/eventlist/EventList";
 import Footer from "../../components/footer/Footer";
 // for build commit
