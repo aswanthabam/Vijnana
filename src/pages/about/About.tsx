0d2043e075e9dd22c5b4a0c67d1ae4c268db2635
Aswanth Vc <aswanth.abam@gmail.com>
2023-11-29 09:54:10 +0530

    about & event page content

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -9,6 +9,26 @@ const About: React.FC<AboutProps> = ({}) => {
     <div className={style.about}>
       <div className={style.page}>
         <h2 className="underline start">About Us</h2>
+        <p>
+          Id ex eu labore magna adipisicing tempor duis elit mollit. Ad sit
+          mollit do laborum mollit. Consequat dolore in culpa exercitation
+          mollit consequat consectetur do sunt proident minim est mollit
+          excepteur. Pariatur aliqua dolor sunt veniam sint est esse commodo
+          irure nulla aliquip minim. Ut duis sint laboris proident excepteur ea
+          ut tempor reprehenderit mollit. Duis irure ad excepteur irure magna
+          aute aliquip et eiusmod duis laboris. Eiusmod quis est aliqua ullamco.
+          Mollit enim sint in nostrud cupidatat veniam. Ex dolore esse quis
+          pariatur duis magna sit aliquip ex non eiusmod aliquip. Aliquip mollit
+          incididunt magna anim non nostrud veniam laboris quis ex proident
+          laboris. Et ad anim ad dolore ut. Magna adipisicing non minim deserunt
+          id tempor exercitation tempor Lorem. Quis pariatur labore do cupidatat
+          reprehenderit ea. Id officia eiusmod tempor dolor dolor amet labore
+          reprehenderit minim aliquip exercitation cillum cillum. Ut cupidatat
+          Lorem anim ad eiusmod veniam. Ut sint sint exercitation labore ipsum
+          labore deserunt veniam cupidatat cupidatat. Nulla tempor enim
+          reprehenderit consectetur ex aliqua esse enim deserunt incididunt ex
+          laborum.
+        </p>
       </div>
       <Footer />
     </div>
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -9,6 +9,29 @@ const Contact: React.FC<ContactProps> = ({}) => {
     <div className={style.contact}>
       <div className={style.page}>
         <h2 className="underline start">Contact Us</h2>
+        <p>
+          Sunt aliqua amet eiusmod duis id velit minim minim. Ut nulla aute in
+          ut sint est aliquip anim labore id elit est magna ipsum. Commodo
+          veniam fugiat officia sit minim magna. Amet culpa ad amet occaecat ad
+          sunt sunt occaecat Lorem. In anim reprehenderit sint duis cillum do
+          laborum ullamco id sint officia Lorem. Ut exercitation sint esse
+          occaecat esse duis aliquip. Ea eu sint cillum proident veniam ea magna
+          deserunt adipisicing commodo cillum. Irure cillum reprehenderit
+          voluptate adipisicing aliqua magna occaecat irure consectetur ut in et
+          sint anim. Consequat culpa nulla incididunt id magna duis sint nulla
+          Lorem ullamco. Nisi anim quis tempor esse sit sint id minim non sunt.
+          Consequat mollit in eiusmod sint duis exercitation laborum cupidatat
+          cillum ad culpa minim veniam ut. Pariatur esse veniam nisi sit aliquip
+          enim. Amet voluptate nisi minim sunt est. Anim consequat voluptate
+          pariatur ex commodo minim amet pariatur aliquip ipsum duis voluptate.
+          Amet laboris qui reprehenderit consectetur incididunt mollit
+          incididunt amet laborum ipsum. Pariatur occaecat pariatur dolor labore
+          proident cupidatat commodo ipsum id cillum sit. Cillum est do eu sint
+          enim laborum. Officia irure deserunt commodo ullamco deserunt amet.
+          Amet velit nulla veniam aliqua anim consectetur sit reprehenderit
+          deserunt. Dolore non adipisicing aliquip aliqua ipsum est id ipsum
+          magna cillum.
+        </p>
       </div>
       <Footer />
     </div>
