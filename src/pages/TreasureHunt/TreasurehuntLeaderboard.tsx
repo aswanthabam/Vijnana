ed4e1c0d4a27beb4e161add769d78baf517ac3d9
Aswanth Vc <aswanth.abam@gmail.com>
2024-03-05 14:54:00 +0530

    feat(treasurehunt) : Treasure hunt UI

---
+++ src/pages/TreasureHunt/TreasurehuntLeaderboard.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { LeaderboardResponse, leaderboard } from "../../apis/treasurehunt";
+import styles from "./TreasureHunt.module.css";
+import { useNavigate } from "react-router-dom";
+
+export const THLeaderboard = () => {
+    const [leaderboardList, setLeaderboardList] = useState<LeaderboardResponse[]>([]);
+    const redirect = useNavigate();
+    useEffect(()=>{
+        leaderboard().then((res)=>{
+            if(res){
+                setLeaderboardList(res);
+            }
+        });
+    },[])
+    return (
+        <div className={styles.container}>
+      <div className={styles.background}>
+        <div className={styles.one}></div>
+        <div className={styles.two}></div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.namePage}>
+            <h1>Leaderboard</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Level</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        leaderboardList?.map((item, index)=>{
+                            return (
+                                <tr>
+                                    <td>{index+1}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.level}</td>
+                                    <td>{new Date(item.time).toLocaleTimeString()}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+            <button onClick={()=>redirect("/th")} className={styles.switchButton}> Go Back</button></div></div>
+        </div>
+    );};
